b918a7086ce0ddc088f1baac222defb9bc0a0bed
zeegy <fred.yuan392@gmail.com>
2025-08-02 16:23:35 -0500

    changed sign_up key

--- frontend_/src/components/sign_up.jsx
+++ frontend_/src/components/sign_up.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
   {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/api/register", {
+      const res = await fetch("https://biblios-backend.onrender.com/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
