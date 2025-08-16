9d8868a67d5c590a1c4ad1bd280c5b8cb9551b13
zeegy <fred.yuan392@gmail.com>
2025-08-15 20:43:43 -0500

    add_keybinds

--- server/add_keybinds.py
+++ server/add_keybinds.py
@@ -10,5 +10,21 @@ conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE Keybinds
+    CREATE TABLE keybinds (
+    username VARCHAR(32) PRIMARY KEY
+    REFERENCES users(username) ON DELETE CASCADE,
+
+    keep_key        TEXT NOT NULL,
+    discard_key     TEXT NOT NULL,
+    donate_key      TEXT NOT NULL,
+    open_chat_key   TEXT NOT NULL,
+    bid_increase_key TEXT NOT NULL,
+    bid_decrease_key TEXT NOT NULL,
+    pass_bid_key     TEXT NOT NULL,
+    place_bid_key    TEXT NOT NULL,
+    take_card_1_key  TEXT NOT NULL,
+    take_card_2_key  TEXT NOT NULL,
+    take_card_3_key  TEXT NOT NULL,
+    take_card_4_key  TEXT NOT NULL
+    );
 """)
