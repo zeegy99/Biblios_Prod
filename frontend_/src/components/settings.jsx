1d2f0254fbf475c6219e978cfc521f0c066eaab3
hiwhatsup01 <zandrew4864@gmail.com>
2025-08-16 15:17:32 -0700

    Remove Test Button

--- frontend_/src/components/settings.jsx
+++ frontend_/src/components/settings.jsx
@@ -249,16 +249,6 @@ const Settings = () => {
         Top Secret Easter Egg Button (DO NOT CLICK)
       </button>
 
-      <button 
-      onClick={() => {
-        const tick = new Audio(Fart);
-        tick.volume = (1);
-        tick.play();
-      }
-      }>
-        Test Button
-      </button>
-
     </div>
 
 
