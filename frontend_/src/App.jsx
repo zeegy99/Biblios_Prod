564a173920825535393ced3605857e899b4ddcd0
zeegy <fred.yuan392@gmail.com>
2025-08-23 17:49:16 -0400

    useeffect impacting deployment

--- frontend_/src/App.jsx
+++ frontend_/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home.jsx";
 import Lobby from "./components/lobby.jsx";
