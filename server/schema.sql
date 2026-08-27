244827e9b66b0650a85e5de72c80c9485140a3ba
zeegy <fred.yuan392@gmail.com>
2026-08-26 22:31:05 -0500

    unique username postgres schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
+    username TEXT UNIQUE NOT NULL,
     email TEXT NOT NULL,
     password_hash TEXT NOT NULL
 );
